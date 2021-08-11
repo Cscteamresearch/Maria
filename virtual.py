477d7c26e971f70eaf87e791e68b33dcbdf7a0a4
silverdan007 <88673671+silverdan007@users.noreply.github.com>
2021-08-11 15:51:37 +0100

    Update virtual.py

--- virtual.py
+++ virtual.py
@@ -6,74 +6,84 @@ import webbrowser
 import os
 import time
 import subprocess
-from ecapture import ecapture as ec
+# from ecapture import ecapture as ec
 import wolframalpha
 import json
 import requests
 
 
-print('Loading your AI personal assistant - NINI')
+print('Loading your AI personal assistant - MAX')
 
-engine=pyttsx3.init('sapi5')
-voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
 
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
         speak("Hello,Good Morning")
         print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Hello,Good Afternoon")
         print("Hello,Good Afternoon")
     else:
         speak("Hello,Good Evening")
         print("Hello,Good Evening")
 
+
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        audio=r.listen(source)
+        audio = r.listen(source)
 
         try:
-            statement=r.recognize_google(audio,language='en-gb')
+            statement = r.recognize_google(audio, language='en-gb')
             print(f"user said:{statement}\n")
 
         except Exception as e:
             return "None"
         return statement
 
-speak("Loading your AI personal assistant NINI")
+
+speak("Loading your AI personal assistant MAX")
 wishMe()
 
 
-if __name__=='__main__':
-
+if __name__ == '__main__':
 
     while True:
+        #speak("Welcome Dr. Bisallah Hashim the H O D of computer science")
+        #speak("Welcome Prof. Abdul-Rasheed Na'Allah the vice chancellor of University of Abuja")
         speak("Tell me how can I help you now?")
         statement = takeCommand().lower()
-        if statement==0:
+        if statement == 0:
             continue
 
         if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant NINI is shutting down,Good bye')
-            print('your personal assistant NINI is shutting down,Good bye')
+            speak('your personal assistant MAX is shutting down,Good bye')
+            print('your personal assistant MAX is shutting down,Good bye')
             break
-
-
 
         if 'wikipedia' in statement:
             speak('Searching Wikipedia...')
-            statement =statement.replace("wikipedia", "")
+            statement = statement.replace("wikipedia", "")
             results = wikipedia.summary(statement, sentences=3)
             speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'google' in statement:
+            speak('Searching Google...')
+            statement = statement.replace("google", "")
+            results = google.summary(statement, sentences=3)
+            speak("According to Google")
             print(results)
             speak(results)
 
@@ -93,15 +103,15 @@ if __name__=='__main__':
             time.sleep(5)
 
         elif "weather" in statement:
-            api_key="8ef61edcf1c576d65d836254e11ea420"
-            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            api_key = "8ef61edcf1c576d65d836254e11ea420"
+            base_url = "https://api.openweathermap.org/data/2.5/weather?"
             speak("whats the city name")
-            city_name=takeCommand()
-            complete_url=base_url+"appid="+api_key+"&q="+city_name
+            city_name = takeCommand()
+            complete_url = base_url+"appid="+api_key+"&q="+city_name
             response = requests.get(complete_url)
-            x=response.json()
-            if x["cod"]!="404":
-                y=x["main"]
+            x = response.json()
+            if x["cod"] != "404":
+                y = x["main"]
                 current_temperature = y["temp"]
                 current_humidiy = y["humidity"]
                 z = x["weather"]
@@ -122,64 +132,61 @@ if __name__=='__main__':
             else:
                 speak(" City Not Found ")
 
-
-
         elif 'time' in statement:
-            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"the time is {strTime}")
 
         elif 'who are you' in statement or 'what can you do' in statement:
-            speak('I am NINI version 1 point O your persoanl assistant. I am programmed to perform minor tasks like'
-                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
+            speak('I am MAX version 1 point O your persoanl assistant. I am programmed to perform minor tasks like'
+                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather'
                   'in different cities , get top headline news from times of USA and you can ask me computational or geographical questions too!')
-
 
         elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
             speak("I was built by Nodestech")
             print("I was built by Nodestech")
-        
+
+        elif "who is the vice chancellor of University of Abuja" in statement:
+            speak("The vice Chancellor of the University of Abuja is Prof. Abdul-Rasheed Na'Allah, he hails from Kwara state in Nigeria")
+            print("The vice Chancellor of the University of Abuja is Prof. Abdul-Rasheed Na'Allah, he hails from Kwara state in Nigeria")
+
+        elif "who is the Head of Department of Computer Science" in statement:
+
+            speak(
+                "The current Head of Department of Computer Science is Doctor Hashim Bisallah")
+            print(
+                "The current Head of Department of Computer Science is Doctor Hashim Bisallah")
+
         elif "open stackoverflow" in statement:
             webbrowser.open_new_tab("https://stackoverflow.com/login")
             speak("Here is stackoverflow")
 
         elif 'news' in statement:
-            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
-            speak('Here are some headlines from the Times of India,Happy reading')
+            news = webbrowser.open_new_tab(
+                "https://www.latestnigeriannews.com/today/vanguard/nigeria-vanguard-newspaper-headlines-today")
+            speak('Here are some headlines news from Nigeria,Happy reading')
             time.sleep(6)
 
-        elif "camera" in statement or "take a photo" in statement:
-            ec.capture(0,"robo camera","img.jpg")
+       # elif "camera" in statement or "take a photo" in statement:
+           # ec.capture(0,"robo camera","img.jpg")
 
-        elif 'search'  in statement:
+        elif 'search' in statement:
             statement = statement.replace("search", "")
             webbrowser.open_new_tab(statement)
             time.sleep(5)
 
         elif 'ask' in statement:
             speak('I can answer to computational and geographical questions and what question do you want to ask now')
-            question=takeCommand()
-            app_id="R2K75H-7ELALHR35X"
+            question = takeCommand()
+            app_id = "R2K75H-7ELALHR35X"
             client = wolframalpha.Client('R2K75H-7ELALHR35X')
             res = client.query(question)
             answer = next(res.results).text
             speak(answer)
             print(answer)
 
-
         elif "log off" in statement or "sign out" in statement:
-            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            speak(
+                "Ok , your pc will log off in 10 sec make sure you exit from all applications")
             subprocess.call(["shutdown", "/l"])
 
 time.sleep(3)
-
-
-
-
-
-
-
-
-
-
-
-
